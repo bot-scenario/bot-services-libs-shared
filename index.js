a2dbcefd161c58d3d342fa8c072d1c267651cc3d
Haim Rubin <haim.rubin@gmail.com>
2023-03-18 19:43:09 +0200

    Changed type to module + prettify

--- index.js
+++ index.js
@@ -3,13 +3,6 @@ export {
   createChanel,
   consumeQueue,
   initializeQueue,
-} from "./src/libs/queue-service.js";
-export {
-  get,
-  post,
-  put,
-  patch,
-  deleteApi,
-  http,
-} from "./src/libs/http/http.js";
-export { HTTP_METHODS } from "./src/libs/http/http-method.js";
+} from './src/libs/queue-service.js'
+export { get, post, put, patch, deleteApi, http } from './src/libs/http/http.js'
+export { HTTP_METHODS } from './src/libs/http/http-method.js'
