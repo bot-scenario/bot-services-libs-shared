d70bf1d320f1995b9cf0450c67608eb3f6108811
Haim Rubin <haim.rubin@gmail.com>
2023-05-01 21:33:04 +0300

    Fixed bug, try to point to client when it's undefined

--- src/libs/redis-om-service/redis-client.js
+++ src/libs/redis-om-service/redis-client.js
@@ -1,12 +1,13 @@
 import { Client } from 'redis-om'
 
 export const redisConnect = async ({ url }) => {
+  let client
   try {
-    const client = new Client(url)
+    client = new Client(url)
     await client.open()
     return client
   } catch (error) {
-    await client.close()
+    await client?.close()
     console.log(error)
     throw error
   }
