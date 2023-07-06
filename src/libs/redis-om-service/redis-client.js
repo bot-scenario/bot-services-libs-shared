f347b765334bb742a52f20f6a034c45ad4c2eeab
Haim Rubin <haim.rubin@gmail.com>
2023-07-06 17:03:04 +0300

    Fixed bud with redis connect

--- src/libs/redis-om-service/redis-client.js
+++ src/libs/redis-om-service/redis-client.js
@@ -3,8 +3,8 @@ import { Client } from 'redis-om'
 export const redisConnect = async ({ url }) => {
   let client
   try {
-    client = new Client(url)
-    await client.open()
+    client = new Client()
+    await client.open(url)
     return client
   } catch (error) {
     if (client) {
