420e1a7a39a3ea8c3998c9c6224be65e1a2a9e42
Haim Rubin <haim.rubin@gmail.com>
2023-07-09 16:49:23 +0300

    Added function to generate graph link + added links to types

--- src/libs/graph/scenario-graph.js
+++ src/libs/graph/scenario-graph.js
@@ -1,39 +1,71 @@
 import Graph from 'graph-data-structure'
-export const PREFIXES = {
-  NEXT: 'next',
-  PREV: 'prev',
-  STAGE: 'stage',
-}
+import {
+  generateNextNodesOfNodeKey,
+  generatePreviousNodesOfNodeKey,
+  generateNextNodesOfStageKey,
+  generatePreviousNodesOfStageKey,
+  generateNextNodeTypeOfPairKey,
+  generatePreviousNodeTypeOfPairKey,
+  generateNextNodesTypeOfNodeKey,
+} from './build-graph-keys.js'
 
 const buildLinks = ({ links, id }, { stage }) => {
   const edges = links.reduce(
     (allBottoms, next) => [
       ...allBottoms,
       {
-        from: `${PREFIXES.NEXT}-${id}`,
+        from: generateNextNodesOfNodeKey({ id }),
         to: next,
       },
       {
-        from: `${PREFIXES.PREV}-${next}`,
+        from: generatePreviousNodesOfNodeKey({ id: next }),
         to: id,
       },
       {
-        from: `${PREFIXES.STAGE}-${stage}-${PREFIXES.NEXT}`,
+        from: generateNextNodesOfStageKey({ stage }),
         to: next,
       },
       {
-        from: `${PREFIXES.STAGE}-${stage}-${PREFIXES.PREV}`,
+        from: generatePreviousNodesOfStageKey({ stage }),
         to: id,
       },
     ],
     [],
   )
+
   return edges
+}
+
+const buildLinksToTypeByPair = ({ links, id }, { nodes }) => {
+  const edges = links.reduce(
+    (allBottoms, next) => [
+      ...allBottoms,
+      {
+        from: generateNextNodeTypeOfPairKey({ id, next }),
+        to: nodes[next].type,
+      },
+      {
+        from: generatePreviousNodeTypeOfPairKey({ id, next }),
+        to: nodes[id].type,
+      },
+    ],
+    [],
+  )
+
+  return edges
+}
+const buildLinksToNextTypeByNode = ({ links, id }, { nodes }) => {
+  const typesEdge = {
+    from: generateNextNodesTypeOfNodeKey({ id }),
+    to: links.map((id) => nodes[id].type),
+  }
+
+  return typesEdge
 }
 
 const recursiveBuildBottoms = ({ head, nodes, stage, list }) => {
   return head.links
-    .map((linkId) => nodes.find((el) => el.id === linkId))
+    .map((linkId) => nodes[linkId])
     .map((head) =>
       buildHierarchyGraph({
         head,
@@ -44,17 +76,26 @@ const recursiveBuildBottoms = ({ head, nodes, stage, list }) => {
     )
 }
 
-const buildHierarchyGraph = ({ head, stage = 0, list = [], nodes }) => {
+const buildHierarchyGraph = ({ head, nodes, stage = 0, list = [] }) => {
   if (!head?.links?.length) {
     return []
   }
 
-  return buildLinks(head, { stage })
-    .concat(recursiveBuildBottoms({ head, nodes, stage, list }))
+  const links = buildLinks(head, { stage })
+  const pairTypeLinks = buildLinksToTypeByPair(head, { nodes })
+  const bottomLinks = recursiveBuildBottoms({ head, nodes, stage, list })
+
+  const typeLink = buildLinksToNextTypeByNode(head, { nodes })
+  const linksJoinedBottoms = []
+    .concat(links)
+    .concat(bottomLinks)
+    .concat(pairTypeLinks)
+    .concat(typeLink)
     .flat()
+
+  return linksJoinedBottoms
 }
-export const createEdges = ({ nodes }, { stage = 0 }) => {
-  const head = nodes.find(({ head }) => head)
+export const createEdges = ({ nodes, head }, { stage = 0 }) => {
   const edges = buildHierarchyGraph({
     head,
     nodes,
@@ -66,7 +107,15 @@ export const createEdges = ({ nodes }, { stage = 0 }) => {
 }
 
 export const createScenarioGraph = ({ nodes, stage }) => {
-  const edges = createEdges({ nodes }, { stage })
+  const nodesHash = nodes.reduce((hash, node) => {
+    return {
+      ...hash,
+      [node.id]: node,
+    }
+  }, {})
+
+  const head = nodes.find(({ head }) => head)
+  const edges = createEdges({ nodes: nodesHash, head }, { stage })
   const graph = edges.reduce(
     (graph, { from, to }) => graph.addEdge(from, to),
     Graph(),
