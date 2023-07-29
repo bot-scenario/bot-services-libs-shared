9c03d7847f33dd0f16187bff39ddeec1291047d0
Haim Rubin <haim.rubin@gmail.com>
2023-07-29 14:31:40 +0300

    Save lastAnswerMessages

--- src/libs/redis-om-service/schema/chat-info.js
+++ src/libs/redis-om-service/schema/chat-info.js
@@ -1,11 +1,18 @@
 import { Entity, Schema } from 'redis-om'
 
-class ChatInfo extends Entity {}
+export class ChatInfo extends Entity {
+  get lastAnswerMessages() {
+    return this.lastAnswerMessagesStringify
+      ? JSON.parse(this.lastAnswerMessagesStringify)
+      : this.lastAnswerMessagesStringify
+  }
+}
 
 const ChatInfoProperties = {
   chatId: { type: 'number' },
   botId: { type: 'string' },
   stage: { type: 'number' },
+  lastAnswerMessagesStringify: { type: 'text' },
 }
 
 export const ChatInfoSchema = new Schema(ChatInfo, ChatInfoProperties, {
