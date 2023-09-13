e76bbd0dfe38c1c0793480aa8ee7c3e5c3b45380
Haim Rubin <haim.rubin@gmail.com>
2023-09-13 17:48:58 +0300

    Added message types for telegram

--- src/constants/telegram-message-types.js
+++ src/constants/telegram-message-types.js
@@ -3,6 +3,10 @@ export const MESSAGE_MEDIA_TYPE = {
   PHOTO: 'photo',
   DOCUMENT: 'document',
   TEXT: 'text',
+  LOCATION: 'location',
+  VOICE: 'voice',
+  VIDEO_NOTE: 'video_note',
+  POLL: 'poll',
 }
 
 export const MESSAGE_TYPE = {
