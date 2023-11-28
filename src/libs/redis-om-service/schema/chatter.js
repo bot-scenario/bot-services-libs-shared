9ab271307e37d06424d8aa4c3c3eeee9327bb1c1
Haim Rubin <haim.rubin@gmail.com>
2023-11-28 14:22:04 +0200

    Added 'dynamicResponse' for chatter

--- src/libs/redis-om-service/schema/chatter.js
+++ src/libs/redis-om-service/schema/chatter.js
@@ -1,6 +1,19 @@
 import { Entity, Schema } from 'redis-om'
 
 export class Chatters extends Entity {
+  #dynamicResponseReady
+  #dynamicResponseReadyStringify
+
+  get dynamicResponse() {
+    if (this.#dynamicResponseReadyStringify !== this.dynamicResponseStringify) {
+      this.#dynamicResponseReady = Graph(
+        JSON.parse(this.dynamicResponseStringify),
+      )
+      this.#dynamicResponseReadyStringify = this.dynamicResponseStringify
+    }
+
+    return this.#dynamicResponseReady
+  }
   get lastAnswerMessages() {
     return this.lastAnswerMessagesStringify
       ? JSON.parse(this.lastAnswerMessagesStringify)
@@ -13,6 +26,7 @@ const ChatterProperties = {
   botId: { type: 'string' },
   stage: { type: 'number' },
   lastAnswerMessagesStringify: { type: 'text' },
+  dynamicResponseStringify: { type: 'string' },
 }
 
 export const ChatterSchema = new Schema(Chatters, ChatterProperties, {
